44956f53e171b237315e6f2f65430558a4af2d15
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-06-24 15:16:05 +0500

    Add NotesClient component and integrate it into the Home page

---
+++ app/components/NotesClient.jsx
@@ -0,0 +1,11 @@
+"use Client"
+
+import React from 'react'
+
+const NotesClient = () => {
+  return (
+    <div>notesClient</div>
+  )
+}
+
+export default NotesClient
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import dbConnect from "@/lib/db";
+import NotesClient from "./components/NotesClient";
+
 
 export default async function Home() {
   await dbConnect();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-3xl font-bold">Notes App</h1>
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Notes App</h1>
+      <NotesClient />
     </main>
   );
 }
