c88646188884ea196abf8799cdcf2f29de756764
hassannawaz1420@gmail.com <hassannawaz1420@gmail.com>
2026-06-24 14:47:28 +0500

    Add Notes model with schema and pre-save hook for updatedAt

---
+++ models/Noute.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const NotesSchema = new mongoose.Schema(
+    {
+    title : {
+        type: String,
+        required: true,
+        maxLength: 100
+    }, 
+
+    content : {
+        type : String,
+        required: true,
+        maxLength: 5000
+    },
+    createdAt : {
+        type : Date,
+        default : Date.now
+    },
+    updatedAt: {
+        type : Date,
+        default : Date.now
+    }
+},
+);
+
+NotesSchema.pre("svae" , function(next){
+    this.updatedAt = Date.now()
+    next()
+})
+
+export default mongoose.model("Note" , NotesSchema)
+
